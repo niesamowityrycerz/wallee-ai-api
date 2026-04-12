6c6de62d34b428516a44c6652f58f0be8b9bffa7
Łukasz Winek <lukasz.winek@prowly.com>
2026-04-12 14:19:28 +0200

    add sentry for staging monitoring

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+return if Rails.env.development?
+
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.sentry.fetch(:dsn)
+  config.environment = Rails.env.production? ? "production" : "staging"
+
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+  config.send_default_pii = Rails.env.production? ? false : true
+
+  config.traces_sample_rate = Rails.application.credentials.sentry.fetch(:traces_sample_rate, 0.0)
+  config.profiles_sample_rate = Rails.application.credentials.sentry.fetch(:profiles_sample_rate, 0.0)
+
+  config.before_send_transaction = lambda do |event, _hint|
+    name = event.transaction
+    next event unless name
+
+    name.include?("/up") ? nil : event
+  end
+end
